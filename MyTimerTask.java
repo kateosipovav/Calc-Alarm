be7fd6c2552d445140638af453609f54eff37454
Ekaterina Ekaterina <eo6165@pleasantonusd.net>
2020-05-28 21:57:14 +0000

    changed the run function to while loop

--- MyTimerTask.java
+++ MyTimerTask.java
@@ -12,12 +12,16 @@ public class MyTimerTask extends TimerTask {
 	public void run() {
     Function.genRandF();
     AudioPlayer ap = new AudioPlayer();
-   do{ 
     ap.ring();
     System.out.println("Enter the answer:::");
-    Scanner input = new Scanner(System.in);  
-    double userAnswer = input.nextDouble(); 
-  }while(Function.answer() != userAnswer); // TODO: compare up to three digits after the comma 
+    Scanner input = new Scanner(System.in);
+    double userAnswer = input.nextDouble();
+    while(Function.answer() != userAnswer){ // TODO: compare up to three digits after the comma 
+     ap.start();
+     System.out.println("Enter the answer:::");
+     Scanner input2 = new Scanner (System.in);
+     double userAnswer = input2.nextDouble(); }
+// copypasted some shit
   System.out.println("Great job!");
   ap.stop();
   
