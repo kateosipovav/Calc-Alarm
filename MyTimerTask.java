2fff0385b8cb7631d25c40dca78bd6ceb36227ef
Ekaterina Ekaterina <eo6165@pleasantonusd.net>
2020-05-29 03:01:39 +0000

    audio settings cjange

--- MyTimerTask.java
+++ MyTimerTask.java
@@ -10,7 +10,6 @@ import javax.sound.sampled.Clip;
 public class MyTimerTask extends TimerTask {
 	MyTimerTask()
 	{
-    System.out.println("Hi from mytimertask");
 		super();
 	}
 
@@ -19,28 +18,25 @@ public class MyTimerTask extends TimerTask {
     System.out.println("Hello from run()");
     Function.genRandF();
    double userAnswer = -1000;
-    
-    //AudioPlayer ap = new AudioPlayer();
-    //ap.ring();
-    ////////////////////////
-    do{ 
 
-     AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("519155__tictac9__inst-1-funk-short-version-tictac9.wav"));
-     Clip clip = AudioSystem.getClip();
-     System.out.println("Enter the answer:::");
+   //////////////////////////////
+  AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("519155__tictac9__inst-1-funk-short-version-tictac9.wav"));
+   Clip clip = AudioSystem.getClip();
+   
+     //Thread.sleep(10000); // looping as long as this thread is alive
+    */
+    do{ 
+     ///////////////////////////
      clip.start(inputStream);
      clip.loop(Clip.LOOP_CONTINUOUSLY);
-     //Thread.sleep(10000); // looping as long as this thread is alive
-
      ///////////////////////////
      System.out.println("Enter the answer:::");
      Scanner input = new Scanner(System.in);
-     userAnswer = input.nextDouble();
+    userAnswer = input.nextDouble();
      }while((int)(Function.answer()*1000) != (int)(userAnswer*1000));
-     //ap.start()
-// copypasted some shit
+
   System.out.println("Great job!");
-  //ap.stop();
+
   clip.stop();
 	}
 }
