c96deebc9c489ede89fc2c2e5aaf85a7e8751f6a
kateosipovav <48306812+kateosipovav@users.noreply.github.com>
2020-05-28 20:50:54 -0700

    Update MyTimerTask.java

--- MyTimerTask.java
+++ MyTimerTask.java
@@ -1,11 +1,9 @@
 import java.util.TimerTask;
 import java.util.*;
 
-import java.io.File;
+import java.io.*;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import javax.sound.sampled.*;
 
 public class MyTimerTask extends TimerTask {
 	MyTimerTask()
@@ -14,29 +12,42 @@ public class MyTimerTask extends TimerTask {
 	}
 
 	@Override
-	public void run() {
-    System.out.println("Hello from run()");
-    Function.genRandF();
-   double userAnswer = -1000;
+	public void run(){
+		try {
+			ring();
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void ring() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		System.out.println("Hello from run()");
+		Function.genRandF();
+		double userAnswer = -1000;
+		
+		//////////////////////////////
+		AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("519155__tictac9__inst-1-funk-short-version-tictac9.wav"));
+		Clip clip = AudioSystem.getClip();
 
-   //////////////////////////////
-  AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("519155__tictac9__inst-1-funk-short-version-tictac9.wav"));
-   Clip clip = AudioSystem.getClip();
-   
-     //Thread.sleep(10000); // looping as long as this thread is alive
-    */
-    do{ 
-     ///////////////////////////
-     clip.start(inputStream);
-     clip.loop(Clip.LOOP_CONTINUOUSLY);
-     ///////////////////////////
-     System.out.println("Enter the answer:::");
-     Scanner input = new Scanner(System.in);
-    userAnswer = input.nextDouble();
-     }while((int)(Function.answer()*1000) != (int)(userAnswer*1000));
+		//Thread.sleep(10000); // looping as long as this thread is alive
+		do{ 
+			///////////////////////////
+			clip.open(inputStream);
+			clip.start();
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+			///////////////////////////
+			System.out.println("Enter the answer:::");
+			//System.out.println(Function.answer());	//prints the answer.. if you're too lazy to actually find the answer
+			Scanner input = new Scanner(System.in);
+			userAnswer = input.nextDouble();
+		}while((int)(Function.answer()*1000) != (int)(userAnswer*1000));
+		
+		System.out.println("Great job!");
 
-  System.out.println("Great job!");
-
-  clip.stop();
+		clip.stop();
 	}
 }
