91a86e284374f7c1e3029373b413ad0a344f13ca
kateosipovav <48306812+kateosipovav@users.noreply.github.com>
2020-05-30 20:31:34 -0700

    Chloe - Final Edits, Hopefully
    just fixed indentation

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 public class Main{
-public static void main(String[] args){
-System.out.println("Enter hours");
-    Scanner input = new Scanner(System.in);  
-    int hours = input.nextInt(); 
-System.out.println("Enter minutes");
-    Scanner input2 = new Scanner(System.in);  
-    int minutes = input2.nextInt(); 
-    Clock c = new Clock(hours, minutes);
-    System.out.println("You are all set!");
-}
+	public static void main(String[] args){
+		System.out.println("Enter hours");
+		Scanner input = new Scanner(System.in);  
+		int hours = input.nextInt(); 
+		System.out.println("Enter minutes");
+		Scanner input2 = new Scanner(System.in);  
+		int minutes = input2.nextInt(); 
+		Clock c = new Clock(hours, minutes);
+		System.out.println("You are all set!");
+	}
 }
